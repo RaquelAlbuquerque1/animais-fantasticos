4cc2a6c37aed0c658ad1a4ccbebdb7753d89fada
RaquelAlbuquerque1 <albuquerqueraquel396@gmail.com>
2021-05-29 19:14:19 -0300

    adicionado debounce a animaçao de scroll

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,12 +1,17 @@
+import debounce from './debounce.js';
+
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
-    this.animaScroll = this.animaScroll.bind(this);
+
+    this.checkDistance = debounce(this.checkDistance.bind(this), 200);
   }
 
+  // Pega a distância de cada item em relação
+  // ao topo do site
   getDistance() {
-    this.distance = [...this.sections].forEach((section) => {
+    this.distance = [...this.sections].map((section) => {
       const offset = section.offsetTop;
       return {
         element: section,
@@ -15,11 +20,13 @@ export default class ScrollAnima {
     });
   }
 
+  // Verifica a distância em cada objeto
+  // em relação ao scroll do site
   checkDistance() {
     this.distance.forEach((item) => {
       if (window.pageYOffset > item.offset) {
         item.element.classList.add('ativo');
-      } else {
+      } else if (item.element.classList.contains('ativo')) {
         item.element.classList.remove('ativo');
       }
     });
@@ -29,12 +36,13 @@ export default class ScrollAnima {
     if (this.sections.length) {
       this.getDistance();
       this.checkDistance();
-      window.addEventListener('scroll', this.animaScroll);
+      window.addEventListener('scroll', this.checkDistance);
     }
     return this;
   }
 
+  // Remove o event de scroll
   stop() {
-    window.removeEventListener('scroll', this.animaScroll);
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
